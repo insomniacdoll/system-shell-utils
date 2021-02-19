2174137b8b347c48225ea6dfbfe6237a23ac4260
insomniacdoll <insomniacdoll@gmail.com>
2021-02-19 14:47:46 +0000

    update

--- dev-tools.sh
+++ dev-tools.sh
@@ -9,7 +9,7 @@
 echo "[DEBUG] INSTALLING BUILD TOOLS"
 sudo apt-get install vim vim-nox zsh
 sudo apt-get install build-essential cmake automake autoconf libtool libtool-bin ctags cscope llvm llvm-dev clang
-sudo apt-get install python-dev python-pip ruby ruby-dev gems nodejs npm libxml2 libxml2-dev libxslt1.1 libxslt-dev lib32z1-dev libgsl-dev libgsl2 gsl-bin libmagickwand-dev imagemagick
+sudo apt-get install python-dev python3-dev python3-pip ruby ruby-dev gems nodejs npm libxml2 libxml2-dev libxslt1.1 libxslt-dev 
 sudo apt-get install astyle tidy
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g js-beautify typescript-formatter
@@ -18,7 +18,8 @@ sudo gem install sass rails sciruby
 mkdir -p ~/.pip
 rm -rf ~/.pip/pip.conf
 cp pip.conf ~/.pip 
-sudo pip install autopep8 django flask flask-restful uwsgi gunicorn twisted tornado scrapy PyYAML poster numpy scipy scikit-learn statsmodels pyodps
+sudo pip3 install virtualenv
+# sudo pip install autopep8 django flask flask-restful uwsgi gunicorn twisted tornado scrapy PyYAML poster numpy scipy scikit-learn statsmodels pyodps
 # sudo apt-get install chromium-browser firefox browser-plugin-freshplayer-pepperflash pepperflashplugin-nonfree
 # sudo apt-get install terminator
 sudo apt-get install git subversion
