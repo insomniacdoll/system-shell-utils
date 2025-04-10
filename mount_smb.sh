2bc59563203a862722ead51317acf6e604e55345
insomniacdoll <insomniacdoll@gmail.com>
2025-04-10 11:04:34 +0800

    refactor

--- mount_smb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-username=$1
-password=$2
-ip=$3
-
-sudo mkdir -p /mnt/WDMYCLOUDEX2
-sudo mount -t cifs //WDMYCLOUDEX2/Private_1 /mnt/WDMYCLOUDEX2 -o user=$username,password=$password,workgroup=WORKGROUP,ip=$ip
-
